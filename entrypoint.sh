8a0a19a50b1817ef89593ad6c2d4ffa23eca06f2
Sinaloense <sinaloensemel@gmail.com>
2025-12-02 19:40:08 -0700

    Fix print startup command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd /home/container
 cloudflared --version
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${PRINT_SAFE_STARTUP}"
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
